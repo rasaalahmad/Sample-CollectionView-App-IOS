db7f5ca7271382c012677065d1c1e7c8a9df1050
Rasaal Ahmad <56187211+rasaalahmad@users.noreply.github.com>
2023-04-25 12:55:58 -0700

    Initial Commit

---
+++ CollectionView App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView App
+//
+//  Created by Rasaal Ahmad on 25/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
