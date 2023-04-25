b36d9075b786de329d4cc7599d546c1cb48d0edf
Rasaal Ahmad <56187211+rasaalahmad@users.noreply.github.com>
2023-04-25 13:20:51 -0700

    Image sets, CollectionCell added

---
+++ CollectionView App/ImagesCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ImagesCollectionViewCell.swift
+//  CollectionView App
+//
+//  Created by Rasaal Ahmad on 25/04/2023.
+//
+
+import UIKit
+
+class ImagesCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ImagesCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+}
--- CollectionView App/ViewController.swift
+++ CollectionView App/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imagesCollecitonView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagesCollecitonView.delegate = self
+        imagesCollecitonView.dataSource = self
+        view.addSubview(imagesCollecitonView)
     }
 
 
 }
 
+
+//MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension ViewController:UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
